1049bc1c6dcce4c5ef15693d0c55a8901c20a390
Keka Dinda <kekadinda21@gmail.com>
2026-08-12 23:36:57 +0530

    35. Search insert position

---
+++ searching/35.py
@@ -0,0 +1,27 @@
+# Search insert position
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if target == nums[mid]:
+                return mid
+
+            elif target > nums[mid]:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return left
+
+solution = Solution()
+
+print(solution.searchInsert([1, 3, 5, 6], 5))  
+print(solution.searchInsert([1, 3, 5, 6], 2))  
+print(solution.searchInsert([1, 3, 5, 6], 7))  
+print(solution.searchInsert([1, 3, 5, 6], 0))  
